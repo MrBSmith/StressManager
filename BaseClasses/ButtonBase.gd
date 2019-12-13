2d408deac677a852d97d80af58c8d4c9061ea669
Hugo Baba <Hugobarette@gmail.com>
2019-12-13 17:38:55 +0100

    Ajout de musique, des sons et du PDF de présentation

--- BaseClasses/ButtonBase.gd
+++ BaseClasses/ButtonBase.gd
@@ -3,6 +3,7 @@ extends TextureButton
 class_name ButtonBase
 
 onready var buttons_node = get_parent()
+onready var blop_node = get_node("../Blop")
 
 var grey_color = "4e4e4e"
 var normal_color = "ffffff"
@@ -13,7 +14,9 @@ func _ready():
 	_err = connect("mouse_entered", self, "on_mouse_entered")
 	_err = connect("mouse_exited", self, "on_mouse_exited")
 	_err = connect("button_up", buttons_node, "on_button_pressed")
+	_err = connect("button_up", blop_node, "on_button_pressed")
 	_err = connect("button_up", self, "on_button_pressed")
+
 
 func on_mouse_entered():
 	set_modulate(normal_color)
